ae4d4b0e528bdab1dc439239a8a2f7ed8049735b
Hugo Daniel <daniel.hugo@protonmail.com>
2021-04-28 17:46:27 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  resources :gardens
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :gardens do
+    resources :plants, only: [:new, :create]
+  end
+
+  resources :plants, only: [:destroy] do
+    member do
+      patch :water
+    end
+  end
+
+  # patch "/plants/:id/water", to: "plants#water"
 end
